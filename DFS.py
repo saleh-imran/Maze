6f748a9041a639fee513969f8f4fdf954cf5b9cf
saleh-imran <110430380+saleh-imran@users.noreply.github.com>
2022-08-24 14:42:52 -0700

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,42 @@
+import collections
+
+
+def hasPath(maze: list[list[int]], start: list[int], destination: list[int]) -> bool:       
+        row, col = len(maze),len(maze[0])
+        queue = collections.deque([(start[0],start[1])])
+        visited = set()
+        dirs = [(-1,0),(0,-1),(1,0),(0,1)]
+        def neighbors(x,y):
+            temp=[]
+            used = set()
+            used.add((x,y))
+            for dx, dy in dirs:
+                nx,ny = x,y
+                while 0 <= nx+dx < row and 0 <= ny+dy < col and maze[nx+dx][ny+dy] == 0:
+                    nx+=dx
+                    ny+=dy
+                if (nx,ny) not in used:
+                    temp.append((nx, ny))
+            return temp
+            
+        while queue:
+            cell = queue.popleft()
+            if cell in visited: continue
+            if cell == (destination[0], destination[1]): return True
+            visited.add(cell)
+            for neighbor in neighbors(cell[0],cell[1]):
+                queue.append(neighbor)                
+        return False
+
+maze=[[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]]
+start1=[0,4]
+destination1=[4,4]
+print(hasPath(maze, start1, destination1))
+
+start2=[0,4]
+destination2=[3,2]
+print(hasPath(maze, start2, destination2))
+
+start3=[4,3]
+destination3=[0,1]
+print(hasPath(maze, start3, destination3))
